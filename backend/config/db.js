cd3cc67f1ff286addeed9d09ae0a7db6a36555c2
Peter Kameel <rtvpeter@gmail.com>
2025-03-18 13:56:19 +0200

    Final Db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connection = mongoose
   .createConnection(
-    "mongodb+srv://peterboles06:f7vfMoXYcxCZ3RiT@shoppingapp.mlufp.mongodb.net/Shopping"
+    "mongodb+srv://project:project123@shoppingapp.mlufp.mongodb.net/Shopping"
   )
   .on("open", () => console.log("Connected to MongoDB"))
   .on("error", (err) => console.log("Error connecting to MongoDB", err));
